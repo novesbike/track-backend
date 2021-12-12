2858d90c55617adf17c423d5c48b6248603fd4e5
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-12-11 21:24:27 -0300

    feat(database): check env before random seed (#17)

--- src/main/java/com/hexagonal/api/database/Seed.java
+++ src/main/java/com/hexagonal/api/database/Seed.java
@@ -4,7 +4,10 @@ import com.hexagonal.api.database.seeds.*;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @AllArgsConstructor
 @Component
@@ -14,15 +17,19 @@ public class Seed implements ApplicationRunner {
     GroupSeeder groupSeeder;
     TrainingSeeder trainingSeeder;
     ActivitySeeder activitySeeder;
+    Environment env;
 
     @Override
     public void run(ApplicationArguments args) {
         roleSeeder.run();
         userSeeder.run();
-        roleSeeder.run(1);
-        userSeeder.run(1);
-        groupSeeder.run(1);
-        trainingSeeder.run(1);
-        activitySeeder.run(1);
+
+        if (Arrays.asList(env.getActiveProfiles()).contains("dev")) {
+            roleSeeder.run(1);
+            userSeeder.run(1);
+            groupSeeder.run(1);
+            trainingSeeder.run(1);
+            activitySeeder.run(1);
+        }
     }
 }
